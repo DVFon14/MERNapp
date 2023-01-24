0a115b53808b8965280bf800678363a9089f7a89
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2023-01-24 10:25:52 -0700

    setup

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+//this is the file where we are going to connect our react
+//application to index.htlm file
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+  .catch((error) => console.log(error.message));
