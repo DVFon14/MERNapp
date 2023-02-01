d62250f2c0151b177b1361846cf6734a975cdd68
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2023-02-01 16:04:54 -0700

    client > api > index > url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 //this is the url pointing to our backend route.
 //this route returns all the posts we have in the database
-const url = 'http://localhost:3000/home';
+const url = 'https://us-states-adventures.onrender.com';
 
 export const fetchHomePosts = () => axios.get(url)
